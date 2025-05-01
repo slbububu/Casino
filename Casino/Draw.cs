7a8103f6a9f9260f5a1f5b1bca2c1d7acbf0f0c3
Jaroslav Zamazal <zamazja@student.uzlabina.cz>
2025-05-01 18:46:39 +0200

    neco jsem zacal delat i guess

---
+++ Casino/Draw.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace Casino
+{
+    public static class Draw
+    {
+        /*
+        public static void Clear()
+        {
+            canvas.Children.Clear(); // background clear +-
+        }*/
+        public static void RenderImage(string filePath, Vector2 position, Vector2 size, float rotation = 0f)
+        {
+            // Load the image
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(filePath, UriKind.RelativeOrAbsolute);
+            bitmap.EndInit();
+
+            // Create the image control
+            Image img = new Image
+            {
+                Source = bitmap,
+                Width = size.X,
+                Height = size.Y,
+                RenderTransformOrigin = new Point(0.5, 0.5)
+            };
+
+            // Apply rotation
+            if (rotation != 0f)
+            {
+                img.RenderTransform = new RotateTransform(rotation);
+            }
+
+            // Position on canvas
+            Canvas.SetLeft(img, position.X);
+            Canvas.SetTop(img, position.Y);
+
+            // Add to canvas
+            MainWindow.GameCanvas.Children.Add(img);
+        }
+
+    }
+}
--- Casino/MainWindow.xaml.cs
+++ Casino/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Reflection.Emit;
+using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Numerics;
 
 namespace Casino
 {
@@ -19,6 +24,45 @@ namespace Casino
         public MainWindow()
         {
             InitializeComponent();
+            CompositionTarget.Rendering += GameLoop; // gameloop bude zavolan pred kazdym framem
+
+            // Fullscreen
+            WindowStyle = WindowStyle.None;
+            WindowState = WindowState.Maximized;
+            ResizeMode = ResizeMode.NoResize;
+        }
+
+        DateTime lastFrameTime = DateTime.Now;
+        public static double deltaTime = 0;
+        public static double toralTime = 0;
+        bool gamePaused = false;
+        bool gamePausedCahnged = false;
+        private void GameLoop(object sender, EventArgs e)
+        {
+            DeltaTime();
+            Update();
+            Render();
+        }
+        private void Update()
+        {
+
+        }
+        private void Render()
+        {
+            canvas.Children.Clear(); // background clear +-
+
+            Draw.RenderImage("Assets/ball.png", new Vector2(100, 200), new Vector2(64, 64), 45f);
+
+        }
+        private void DeltaTime()
+        {
+            deltaTime = (DateTime.Now - lastFrameTime).TotalSeconds;
+            lastFrameTime = DateTime.Now;
+
+            //testing (debug mod je nepouzitelny bez tohoto)
+            //deltaTime = 0.1;
+
+            toralTime += deltaTime;
         }
     }
 }
